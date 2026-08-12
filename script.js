9277a73fcaf35c18302eb06759f7dce1eac07252
villa-ser <sergiovillagraelectricista@gmail.com>
2026-08-12 15:19:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// villaser.github.io - Lógica JS
+// Inicialización del entorno web
 
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Villaser Gestión: Interfaz cargada correctamente.");
+    
+    // El espacio está listo para agregar scripts futuros
+    // Ej: Calculadoras de Icc, caída de tensión o validación de formularios.
+});
